298206745795904c8b90c8bfb5b3894e9d150964
Stephen Blais <stephen@s-blais.com>
2019-08-19 21:15:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,14 +3,14 @@ def badge_maker (name)
 end
 
 def batch_badge_creator (attendees)
-  attendees.collect |attendee| do
+  attendees.collect do |attendee|
     name = attendee
     badge_maker (attendee)
   end
 end
 
 def assign_rooms (attendees)
-  attendees.each_with_index |name, index| do
+  attendees.each_with_index do |name, index|
 
     room_assignments.push ("Hello, #{name}! You'll be assigned to room #{index+1}!")
   end
