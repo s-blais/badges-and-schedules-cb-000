db29ed2dd8a8e586369ab2d54d8d77009b57ebdc
Stephen Blais <stephen@s-blais.com>
2019-08-19 22:09:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker (name)
 end
 
 def batch_badge_creator (attendees)
-  #badge_batch = [] (this isn't needed?)
+  badge_batch = []
   attendees.each do |attendee|
     name = attendee
     badge_batch.push (badge_maker (name))
@@ -12,7 +12,7 @@ def batch_badge_creator (attendees)
 end
 
 def assign_rooms (attendees)
-  #room_assignments = [] (this isn't needed?)
+  room_assignments = []
   attendees.each_with_index do |name, index|
     room_assignments.push ("Hello, #{name}! You'll be assigned to room #{index+1}!")
   end
