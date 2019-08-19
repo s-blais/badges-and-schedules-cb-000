d4a6b685f96bec0c0bbcad24c1ed07596ff1e461
Stephen Blais <stephen@s-blais.com>
2019-08-19 21:27:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,6 @@ end
 def assign_rooms (attendees)
   room_assignments = []
   attendees.each_with_index do |name, index|
-
     room_assignments.push ("Hello, #{name}! You'll be assigned to room #{index+1}!")
   end
   room_assignments
