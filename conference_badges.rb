533bc3a3a0c3357054211988ea368060638fd8cd
Stephen Blais <stephen@s-blais.com>
2019-08-19 21:13:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker (name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (attendees)
+  attendees.collect |attendee| do
+    name = attendee
+    badge_maker (attendee)
+  end
+end
+
+def assign_rooms (attendees)
+  attendees.each_with_index |name, index| do
+
+    room_assignments.push ("Hello, #{name}! You'll be assigned to room #{index+1}!")
+  end
+  room.assignments
+end
