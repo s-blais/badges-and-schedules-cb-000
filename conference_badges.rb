dbec496c3a7c6bf0a2e05f6e4becbd5f0471395e
Stephen Blais <stephen@s-blais.com>
2019-08-19 22:03:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,8 +23,7 @@ def printer (attendees)
   (batch_badge_creator (attendees)).each do |badge|
     puts badge
   end
-  assign_rooms (attendees)
-  room_assignments.each do |room_assignment|
+  (assign_rooms (attendees)).each do |room_assignment|
     puts room_assignment
   end
 end
